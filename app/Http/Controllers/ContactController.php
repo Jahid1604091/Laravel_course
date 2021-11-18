a16a54d979bdd2019784ab021da7bf9ae7a2725b
Jahid1604091 <jh409780@gmail.com>
2021-11-18 18:40:02 +0600

    Passing data through controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
 
 class ContactController extends Controller
 {
     public function index()
     {
+        // --step 1--
         // return "<h1>This a Contact controller</h1>";
-        return view('contact/index');
+
+        //--step 2--
+        // $name = "Jahid Hasan";
+        // return view('contact/index',['name'=>$name]);
+
+        //--step 3--
+        $today = date("F j, Y, g:i a");
+        $data = [
+            "creator" => "jahid Hasan",
+            "date" => $today,
+            "contact_no" => [
+                 "01715150941",
+                "01918219152"
+            ],
+
+        ];
+        return view('contact.index', $data);
     }
 }
--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,1 +1,12 @@
-<h2>This is contact page</h2>
+<h2>This is contact page created by <strong>{{$creator}}</strong> on {{$date}} </h2>
+<h3>Contact him at -
+    <ol>
+        @foreach($contact_no as $contact)
+            <li>{{$contact}}</li>
+        @endforeach
+    </ol>
+
+</h3>
+
+
+
