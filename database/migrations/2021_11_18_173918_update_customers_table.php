8d39f79be07172dc51ba235a75ea62f65f8cff5b
Jahid1604091 <jh409780@gmail.com>
2021-11-18 23:53:24 +0600

    All About Migration

---
+++ database/migrations/2021_11_18_173918_update_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->string('name',255)->after('id');
+            $table->string('designation',20)->change();
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
+    }
+}
